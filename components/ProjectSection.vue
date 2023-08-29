2e011018f97c61ee3d96d32b43650d1715f100b0
Thi Khanh Uyen Tran <traty145@mymail.unisa.edu.au>
2023-08-29 18:30:13 +0930

    fix image name

--- components/ProjectSection.vue
+++ components/ProjectSection.vue
@@ -4,12 +4,12 @@
     <!--Project cards-->
     <v-row>
       <v-col cols="12" md="6">
-        <ProjectCard imgPath="./showcase1.JPG" imgPath1="./showcase11.jpg" imgPath2="./showcase12.jpg" title="Business Landing Page" subtitle="Nuxt3 + Vuetify"
+        <ProjectCard imgPath="./showcase1.JPG" imgPath1="./showcase11.JPG" imgPath2="./showcase12.JPG" title="Business Landing Page" subtitle="Nuxt3 + Vuetify"
           projectUrl="https://github.com/UyenTran218/Movusx-app-nuxt-3"
           description="Cutting-edge Business Landing Page using Vuetify's material design and Nuxt.js's server-side rendering capabilities. The aim of this project was to create an aesthetically pleasing, yet highly functional page that serves as the first point of contact between the business and potential clients" />
       </v-col>
       <v-col cols="12" md="6">
-        <ProjectCard imgPath="./showcase2.JPG" imgPath1="./showcase21.jpg" imgPath2="./showcase22.jpg" title="POS System Interface" subtitle="Typescript + Vue3 + Vuetify"
+        <ProjectCard imgPath="./showcase2.JPG" imgPath1="./showcase21.JPG" imgPath2="./showcase22.JPG" title="POS System Interface" subtitle="Typescript + Vue3 + Vuetify"
           projectUrl="https://github.com/UyenTran218/Restaurant-app"
           description="Streamlined POS (Point-of-Sale) system tailored for a restaurant setting. This web application empowers restaurant staff to easily manage orders, from item selection to billing and print/save functionalities. The application is built with Nuxt.js using TypeScript for type safety, and it incorporates Vuetify’s Material Design for an elegant, yet intuitive, user interface." />
       </v-col>
