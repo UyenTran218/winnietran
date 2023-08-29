e1a9a28c68cb79f6f3692f23a9c9deeaf26710f8
Thi Khanh Uyen Tran <traty145@mymail.unisa.edu.au>
2023-08-29 17:28:49 +0930

    add carousel to display showcase images

--- components/ProjectCard.vue
+++ components/ProjectCard.vue
@@ -1,11 +1,22 @@
 <template>
   <v-card class="my-6" min-width="200px">
     <!--Project overview-->
-    <v-img :src="imgPath" cover style="filter: grayscale(1);"></v-img>
-    <v-card-title class="card-title"> {{ title }} </v-card-title>
-    <v-card-subtitle> {{ subtitle }} </v-card-subtitle>
+    <v-carousel hide-delimiters show-arrows="hover" height="300">
+      <v-carousel-item>
+        <v-img :src="imgPath" cover style="filter: grayscale(1);"></v-img>
+      </v-carousel-item>
+      <v-carousel-item>
+        <v-img :src="imgPath1" cover style="filter: grayscale(1);"></v-img>
+      </v-carousel-item>
+      <v-carousel-item>
+        <v-img :src="imgPath2" cover style="filter: grayscale(1);"></v-img>
+      </v-carousel-item>
+    </v-carousel>
+    
+    <v-card-title class="card-title my-4"> {{ title }} </v-card-title>
+    <v-card-subtitle class="my-3"> {{ subtitle }} </v-card-subtitle>
     <v-card-actions>
-      <v-btn color="#768a82" variant="tonal" :href="projectUrl" target="_blank"> Github </v-btn>
+      <v-btn color="#94ada3" variant="tonal" :href="projectUrl" target="_blank"> Github </v-btn>
       <v-spacer></v-spacer>
       <v-btn :icon="show ? 'mdi-chevron-up' : 'mdi-chevron-down'" @click="show = !show"></v-btn>
     </v-card-actions>
@@ -13,7 +24,7 @@
     <v-expand-transition>
       <div v-show="show">
         <v-divider></v-divider>
-        <v-card-text>
+        <v-card-text class="card-text">
           <p>
             {{ description }}
           </p>
@@ -31,6 +42,8 @@ export default {
   }),
   props: {
     imgPath: String,
+    imgPath1: String,
+    imgPath2: String,
     title: String,
     subtitle: String,
     description: String,
@@ -43,6 +56,11 @@ export default {
   white-space: normal;
   text-overflow: clip;
   font-size: 18px;
+  background-color: #94ada3;
+  color: white;
+}
+.card-text {
+  height: 300px;
 }
 </style>
   
